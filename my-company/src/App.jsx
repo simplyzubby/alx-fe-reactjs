01f1eec2e9adf10100f702e4470938a08febecd6
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-25 00:15:50 +0100

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from "react";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route,} from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
