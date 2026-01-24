d8ae4f31da04aebeb5aa192062aa0a929408bfc8
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-24 12:09:10 +0100

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,6 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import Navbar from './pages/Navbar'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
