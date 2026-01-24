d8927318b0794fede715cbb23188e9fe797a2148
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-24 10:49:48 +0100

    update counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,7 +39,9 @@ function App() {
  <Footer />
  <MainContent />
  <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+  <Counter />
      </>
+
   )
 }
 
