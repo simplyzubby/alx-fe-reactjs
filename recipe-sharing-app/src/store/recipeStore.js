225770d3fab00685cb291754cafbf81ca8220c41
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-26 11:38:36 +0100

    update

--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,6 +1,6 @@
 import create from 'zustand'
 
-const useRecipeStore = create(set => ({
+const recipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
