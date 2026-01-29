394bf7583a079ea079ba0320130f71b37d9f55e0
Zubaida <zubaidasuleiman89@gmail.com>
2026-01-29 08:02:05 +0100

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,23 +1,31 @@
- import { useRecipeStore } from './recipeStore';
- import { Link } from 'react-router-dom';
- 
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
+import AddRecipeForm from './AddRecipeForm';
+import SearchBar from './SearchBar';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const filteredRecipes = useRecipeStore(
+    (state) => state.filteredRecipes
+  );
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-             <Link to={`/recipes/${recipe.id}`}>
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+
+      <SearchBar />
+      <AddRecipeForm />
+
+      {filteredRecipes.length === 0 && <p>No recipes found</p>}
+
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <Link to={`/recipes/${recipe.id}`}>
             {recipe.title}
           </Link>
-          </div>
-        ))}
-      </div>
-    );
-  };
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default RecipeList;
-  
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,20 +1,23 @@
-export const useRecipeStore = create((set) => ({
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set, get) => ({
   recipes: [],
+
+  searchTerm: '',
+  filteredRecipes: [],
 
   addRecipe: (recipe) =>
     set((state) => ({
       recipes: [...state.recipes, recipe],
+      filteredRecipes: [...state.recipes, recipe],
     })),
-  
-  setRecipes: (recipes) =>
-    set(() => ({
-      recipes,
-    })),
-
 
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+      filteredRecipes: state.filteredRecipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
@@ -22,7 +25,20 @@ export const useRecipeStore = create((set) => ({
   deleteRecipe: (id) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      filteredRecipes: state.filteredRecipes.filter(
+        (recipe) => recipe.id !== id
+      ),
+    })),
+
+  setSearchTerm: (term) => {
+    set({ searchTerm: term });
+    get().filterRecipes();
+  },
+
+  filterRecipes: () =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
+        recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+      ),
     })),
 }));
-
- setRecipes: (recipes) => set({ recipes })
