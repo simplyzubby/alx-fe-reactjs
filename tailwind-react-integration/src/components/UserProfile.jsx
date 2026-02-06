f78612acd1610480cf2d52dbc51a85ac7a76816c
Zubaida <zubaidasuleiman89@gmail.com>
2026-02-06 20:56:04 +0100

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:mx-w mx-w-xs md:mx-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:mx-w max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" alt="User" class= "rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto"/>
       <h1 class = "md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
       <p class="text-gray-600 md:text-base sm:text-sm">Developer at Example Co. Loves to write code and explore new technologies.</p>
